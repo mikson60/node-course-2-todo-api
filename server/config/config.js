6e30dd5f993d4262ae8a0464025ac27a69fab829
Jens (Aorus) <mikson60@gmail.com>
2018-05-27 21:54:28 +0300

    Updated production environment config setup

--- server/config/config.js
+++ server/config/config.js
@@ -2,23 +2,14 @@ var env = process.env.NODE_ENV || 'development';
 
 console.log('env *****', env);
 
-var config = require('./config.json');
-
 if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
     var envConfig = config[env];
 
     Object.keys(envConfig).forEach((key) => {
         process.env[key] = envConfig[key];
     });
-} else if (env === 'production') {
-    // var envConfig = config[env];
-
-    // Object.keys(envConfig).forEach((key) => {
-    //     process.env[key] = envConfig[key];
-    // });
-    process.env.MONGODB_URI = 'mongodb://admin:admin@ds235180.mlab.com:35180/todo-app-db';
 }
-
 
 // if (env === 'production') {
 //     process.env.MONGODB_URI = 'mongodb://admin:admin@ds235180.mlab.com:35180/todo-app-db';
